4fb27c80edd422f26d0378b4f18a377e39cc7053
tsubus <tsubus@ilwt.org>
2014-01-17 10:41:41 +0100

    Add SuperSu and Substratum to settings dashboard
    Change-Id: Ifb5bb4c87744ef891d2a6ab1ede1af40bb14fdf8

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -160,4 +160,8 @@ public class Settings extends SettingsActivity {
     public static class DeviceSettings extends SettingsActivity { /* empty */ }
     public static class PersonalSettings extends SettingsActivity { /* empty */ }
     public static class SystemSettings extends SettingsActivity { /* empty */ }
+
+    // Bliss Additions
+    public static class SubstratumActivity extends SettingsActivity { /* empty */ }
+    public static class SuperSUActivity extends SettingsActivity { /* empty */ }
 }
